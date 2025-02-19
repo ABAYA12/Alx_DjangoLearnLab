148d13cc94aef146f787240e9503d4bdb2845988
Ishmael Kabu Abayateye <123239306+ABAYA12@users.noreply.github.com>
2025-02-19 13:57:28 +0000

    added files

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+admin.site.register(Book)
+list_display = ("title", "author", "publication_year")
+search_fields = ("title", "author")
