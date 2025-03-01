eb422f9c4410d09705160d0601372ce014bf6189
Ishmael Kabu Abayateye <123239306+ABAYA12@users.noreply.github.com>
2025-03-01 21:45:52 +0000

    added files

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -6,4 +6,3 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")  # Columns shown in the admin list view
     list_filter = ("publication_year", "author")  # Filters for easy navigation
     search_fields = ("title", "author")  # Search functionality
-
