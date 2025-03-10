3add15813f73c066d07ec96418ace616182b9b2d
unknown <you@example.com>
2025-03-10 16:09:10 +0000

    added files

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import LibraryDetailView
 from .views import list_books
+from .views import register_view, login_view, logout_view
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", list_books, name="list_books"),
     path("library/<int:pk>", LibraryDetailView.as_view(), name="library_detail"),
+    path("register/", register_view, name="register"),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import AuthenticationForm
 from .models import Book,Author, Librarian
 from .models import Library
+from .forms import RegisterForm
 
 # Create your views here.
 def list_books(request):
@@ -18,3 +21,33 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context["books_in_library"] = self.object.books.all()
         return context
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")  # Redirect to a home page after registration
+    else:
+        form = RegisterForm()
+    return render(request, "register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("home")  # Redirect to home after login
+    else:
+        form = AuthenticationForm()
+    return render(request, "login.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return render(request, "logout.html")
