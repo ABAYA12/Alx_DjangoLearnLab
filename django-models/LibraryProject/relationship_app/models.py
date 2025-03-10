b2945581dfe0a436e42b64875c1bc7998043f391
unknown <you@example.com>
2025-03-10 15:36:29 +0000

    change models

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,14 +3,22 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100) 
+    def __str__(self):
+        return self.name
 class Book(models.Model):
     tittle = models.CharField(max_length=100, default='No Title')
     author = models.ManyToManyField(Author, max_length=100)
+    def __str__(self):
+        return self.tittle
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book, max_length=100)
+    def __str__(self):
+        return self.name
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, max_length=100)
+    def __str__(self):
+        return self.name
 
 
